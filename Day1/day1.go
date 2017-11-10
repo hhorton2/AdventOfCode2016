67aafc3694680faafeadaf9fec65b8e53da1f26f
Hunter Horton <hunterhorton89@gmail.com>
2017-11-09 22:27:36 -0600

    Add input and solve day 1

--- Day1/day1.go
+++ Day1/day1.go
@@ -2,25 +2,138 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"strings"
 	"strconv"
-	"sync"
+	"math"
 )
 
 func main() {
-	wg := &sync.WaitGroup{}
-	hellos := make(chan string, 20)
-	for i := 0; i < 20; i++ {
-		wg.Add(1)
-		go getHello(i, hellos, wg)
+	dat, err := ioutil.ReadFile("Day1/input.txt")
+	check(err)
+	dataArray := strings.Split(strings.Replace(string(dat), "\n", "", -1), ", ")
+	solvePartOne(dataArray)
+	solvePartTwo(dataArray)
+}
+func solvePartOne(dataArray []string) {
+	North := new(Direction)
+	East := new(Direction)
+	South := new(Direction)
+	West := new(Direction)
+	North.left = West
+	North.right = East
+	East.left = North
+	East.right = South
+	South.left = East
+	South.right = West
+	West.left = South
+	West.right = North
+	currentDirection := North
+	for _, instruction := range dataArray {
+		direction := getDirection(instruction)
+		steps := getSteps(instruction)
+		if direction == "R" {
+			currentDirection = currentDirection.right
+		} else {
+			currentDirection = currentDirection.left
+		}
+		currentDirection.steps += steps
 	}
-	wg.Wait()
-	close(hellos)
-	for greeting := range hellos {
-		fmt.Println(greeting)
+	fmt.Printf("north: %v, east: %v, south: %v, west: %v\n", North.steps, East.steps, South.steps, West.steps)
+	fmt.Printf("blocks away: %v\n", math.Abs(float64(North.steps-South.steps))+math.Abs(float64(East.steps-West.steps)))
+}
+func solvePartTwo(dataArray []string) {
+	North := new(Direction)
+	East := new(Direction)
+	South := new(Direction)
+	West := new(Direction)
+	North.left = West
+	North.right = East
+	East.left = North
+	East.right = South
+	South.left = East
+	South.right = West
+	West.left = South
+	West.right = North
+	currentDirection := North
+	var grid []Vertex
+	currentVertex := Vertex{x: 0, y: 0}
+	grid = append(grid, currentVertex)
+	foundHq := false
+	for _, instruction := range dataArray {
+		direction := getDirection(instruction)
+		steps := getSteps(instruction)
+		if direction == "R" {
+			currentDirection = currentDirection.right
+		} else {
+			currentDirection = currentDirection.left
+		}
+		for i := 0; i < steps; i++ {
+			currentDirection.steps = currentDirection.steps + 1
+			newVertex := currentVertex
+			switch currentDirection {
+			case North:
+				foundHq = checkIfAlreadyVisited(grid, Vertex{x: currentVertex.x, y: currentVertex.y + 1}, foundHq)
+				newVertex.y += 1
+			case East:
+				foundHq = checkIfAlreadyVisited(grid, Vertex{x: currentVertex.x + 1, y: currentVertex.y}, foundHq)
+				newVertex.x += 1
+			case South:
+				foundHq = checkIfAlreadyVisited(grid, Vertex{x: currentVertex.x, y: currentVertex.y - 1}, foundHq)
+				newVertex.y -= 1
+			default:
+				foundHq = checkIfAlreadyVisited(grid, Vertex{x: currentVertex.x - 1, y: currentVertex.y}, foundHq)
+				newVertex.x -= 1
+			}
+			if foundHq {
+				break
+			} else {
+				grid = append(grid, newVertex)
+				currentVertex = newVertex
+			}
+		}
+		if foundHq {
+			break
+		}
+	}
+
+	fmt.Printf("north: %v, east: %v, south: %v, west: %v\n", North.steps, East.steps, South.steps, West.steps)
+	fmt.Printf("blocks away: %v\n", math.Abs(float64(North.steps-South.steps))+math.Abs(float64(East.steps-West.steps)))
+}
+func checkIfAlreadyVisited(grid []Vertex, checkVertex Vertex, foundHq bool) bool {
+	for _, vertex := range grid {
+		if vertex.x == checkVertex.x && vertex.y == checkVertex.y {
+			foundHq = true
+			break
+		}
+	}
+	return foundHq
+}
+func getSteps(instruction string) int {
+	steps, err := strconv.Atoi(instruction[1:])
+	if err != nil {
+		panic(err)
+	} else {
+		return steps
+	}
+}
+func getDirection(instruction string) string {
+	return instruction[0:1]
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
 	}
 }
 
-func getHello(i int, hellos chan<- string, wg *sync.WaitGroup) {
-	defer wg.Done()
-	hellos <- "Hello World: " + strconv.Itoa(i)
+type Direction struct {
+	right *Direction
+	left  *Direction
+	steps int
+}
+
+type Vertex struct {
+	x int
+	y int
 }
