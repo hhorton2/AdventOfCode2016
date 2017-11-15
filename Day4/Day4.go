c83931a103c3612294315271f8899e3b11eb4a87
Hunter Horton <hunterhorton89@gmail.com>
2017-11-14 19:01:50 -0600

    checking in for gopher help with race condition.

--- Day4/Day4.go
+++ Day4/Day4.go
@@ -6,6 +6,8 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
+	"sync"
+	"sort"
 )
 
 func main() {
@@ -13,9 +15,55 @@ func main() {
 	check(err)
 	datString := string(dat)
 	rooms := getRooms(datString)
+	wg := new(sync.WaitGroup)
+	sectorChan := make(chan int, len(rooms))
 	for _, room := range rooms {
-		fmt.Printf("%v | %v | %v\n", room.encryptedName, room.sectorId, room.checksum)
+		//fmt.Printf("%v | %v | %v\n", room.encryptedName, room.sectorId, room.checksum)
+		wg.Add(1)
+		go getSectorIfValid(room, wg, sectorChan)
 	}
+	wg.Wait()
+	close(sectorChan)
+	sectorSum := 0
+	for sector := range sectorChan {
+		sectorSum += sector
+	}
+	fmt.Println(sectorSum)
+}
+func getSectorIfValid(room Room, wg *sync.WaitGroup, sectors chan<- int) {
+	defer wg.Done()
+	letters := getMapFromRoomName(room.encryptedName)
+	actualChecksum := getChecksumFromMap(letters)
+	if actualChecksum == room.checksum {
+		sectors <- room.sectorId
+	}
+}
+func getMapFromRoomName(roomName string) map[string]int {
+	letters := map[string]int{}
+	for _, currentRune := range roomName {
+		currentLetter := strings.Replace(strconv.QuoteRune(currentRune), "'", "", -1)
+		letters[currentLetter] = letters[currentLetter] + 1
+	}
+	return letters
+}
+
+func getChecksumFromMap(letters map[string]int) string {
+	var ss []kv
+	for k, v := range letters {
+		ss = append(ss, kv{k, v})
+	}
+
+	sort.Slice(ss, func(i, j int) bool {
+		if ss[i].Value == ss[j].Value {
+			return ss[i].Key > ss[i].Key
+		}
+		return ss[i].Value > ss[j].Value
+	})
+	var str string
+	for i := 0; i < 5; i++ {
+		str += ss[i].Key
+	}
+	return str
 }
 func getRooms(datString string) ([]Room) {
 	regex := regexp.MustCompile(".*\n")
@@ -59,4 +107,9 @@ type Room struct {
 	checksum      string
 	sectorId      int
 	encryptedName string
+}
+
+type kv struct {
+	Key   string
+	Value int
 }
